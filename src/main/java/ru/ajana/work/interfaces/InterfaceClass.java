749856238b7a74175c28f0ab3f372b75bffdeba4
Andrey Kharintsev <andrej738@gmail.com>
2018-10-04 23:18:49 +0500

    fix

---
+++ src/main/java/ru/ajana/work/interfaces/InterfaceClass.java
@@ -0,0 +1,21 @@
+package ru.ajana.work.interfaces;
+
+/**
+ * @author Andrey Kharintsev on 18.07.2018
+ */
+public interface InterfaceClass {
+
+  public abstract String func1();
+
+  public String func2();
+
+  public String func3();
+
+  default String func4() {
+    return "123";
+  }
+
+  static String func5() {
+    return "123";
+  }
+}
