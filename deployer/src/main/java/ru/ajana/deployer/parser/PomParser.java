567162d6e94bcccf02b7e289e681da5b5e848900
Andrey Kharintsev <AKharintsev@ibs.ru>
2019-05-29 20:14:47 +0500

    Поправил зависимость

--- deployer/src/main/java/ru/ajana/deployer/parser/PomParser.java
+++ deployer/src/main/java/ru/ajana/deployer/parser/PomParser.java
@@ -1,13 +1,13 @@
 package ru.ajana.deployer.parser;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import javax.xml.transform.stax.StAXSource;
 import org.apache.commons.io.IOUtils;
-import org.apache.maven.pom._4_0.Model;
+import org.apache.maven.model.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Парсер Maven POM.XML
@@ -16,19 +16,19 @@ import org.slf4j.LoggerFactory;
  */
 public class PomParser {
 
-  private static final Logger LOG = LoggerFactory.getLogger(PomParser.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PomParser.class);
 
-  public static void main(String[] args) {
-    try {
+    public static void main(String[] args) {
+        try {
 
-      InputStream in = PomParser.class.getClassLoader().getResourceAsStream("pom/sample-pom.xml");
-      String xml = IOUtils.toString(in, "UTF-8");
-      //Model model = JAXB.unmarshal(in, Model.class);
-      Model model = UnmarshalUtils.unmarshal(xml, "project", Model.class);
-      LOG.info("version: {}", model.getVersion());
+            InputStream in = PomParser.class.getClassLoader().getResourceAsStream("pom/sample-pom.xml");
+            String xml = IOUtils.toString(in, "UTF-8");
+            //Model model = JAXB.unmarshal(in, Model.class);
+            Model model = UnmarshalUtils.unmarshal(xml, "project", Model.class);
+            LOG.info("version: {}", model.getVersion());
 
-    } catch (IOException e) {
-      e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
